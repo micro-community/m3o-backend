4c7d2f0c24e22726ed4fab7dd0dd8419f4697377
Ben Toogood <bentoogood@gmail.com>
2020-05-28 16:59:12 +0100

    Build

---
+++ home/web/app/build/precache-manifest.0d2905c3cd92f781fac33a0e3daa0d72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc34dafd5ff7cae0a037ab12b02a6b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38fdc28c0ab9c6bd8ab1",
+    "url": "/static/css/main.df4817c3.chunk.css"
+  },
+  {
+    "revision": "aef0d23ee66729bdb04d",
+    "url": "/static/js/2.38bec15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38bec15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fdc28c0ab9c6bd8ab1",
+    "url": "/static/js/main.02e8943b.chunk.js"
+  },
+  {
+    "revision": "dfd182da2461ddda8af9",
+    "url": "/static/js/runtime-main.f258c7a8.js"
+  },
+  {
+    "revision": "9d58c7d899ffffc36d9b4c5acd5c812d",
+    "url": "/static/media/CI.9d58c7d8.svg"
+  },
+  {
+    "revision": "2c0eb81b1b94213b00fc84228f94a921",
+    "url": "/static/media/curved.2c0eb81b.svg"
+  },
+  {
+    "revision": "c29c76e742ca6aa92d61d510c8572e4b",
+    "url": "/static/media/header.c29c76e7.png"
+  }
+]);
---
+++ home/web/go.mod
@@ -0,0 +1,5 @@
+module github.com/micro/services/home/web
+
+go 1.13
+
+require github.com/micro/go-micro/v2 v2.7.0
